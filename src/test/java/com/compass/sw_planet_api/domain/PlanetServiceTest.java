1164542d878195ae1ff62fcb9f561d28af9e903d
Luis Eduardo <luixst1@gmail.com>
2025-02-07 15:11:37 -0300

    [feat] Testando com mockito

--- src/test/java/com/compass/sw_planet_api/domain/PlanetServiceTest.java
+++ src/test/java/com/compass/sw_planet_api/domain/PlanetServiceTest.java
@@ -2,20 +2,29 @@ package com.compass.sw_planet_api.domain;
 
 import static com.compass.sw_planet_api.common.PlanetConstants.PLANET;
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import com.compass.sw_planet_api.repository.PlanetRepository;
 import com.compass.sw_planet_api.service.PlanetService;
+@ExtendWith(MockitoExtension.class)
 
-@SpringBootTest(classes = PlanetService.class)
 public class PlanetServiceTest {
 
-    @Autowired
+    @InjectMocks
     private PlanetService planetService;
+    @Mock
+    private PlanetRepository planetRepository;
 
     @Test
     public void createPlanet_WithValidData_ReturnsPlanet() {
+        when(planetRepository.save(PLANET)).thenReturn(PLANET);
+
         Planet sut = planetService.create(PLANET);
 
         assertThat(sut).isEqualTo(PLANET);
