18b3b260f6b39701dc3493174ef5da67c05112b4
Luis Eduardo <luixst1@gmail.com>
2025-02-09 23:54:02 -0300

    [feat] Camada de testes de integração

--- src/main/java/com/compass/sw_planet_api/domain/Planet.java
+++ src/main/java/com/compass/sw_planet_api/domain/Planet.java
@@ -2,6 +2,7 @@ package com.compass.sw_planet_api.domain;
 
 import org.apache.commons.lang3.builder.EqualsBuilder;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -11,47 +12,80 @@ import jakarta.persistence.Table;
 @Entity
 @Table(name = "planets")
 public class Planet {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
-    
-    private String name;
-    private String terrain;
-    private String climate;
+  @Column(nullable = false, unique = true)
+  private String name;
 
-    public Planet(String name, String terrain) {
-        this.name = name;
-        this.terrain = terrain;
-    }
+  @Column(nullable = false)
+  private String climate;
 
-    public Long getId() {
-        return id;
-    }
-    public void setId(Long id) {
-        this.id = id;
-    }
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-    public String getTerrain() {
-        return terrain;
-    }
-    public void setTerrain(String terrain) {
-        this.terrain = terrain;
-    }
-    public String getClimate() {
-        return climate;
-    }
-    public void setClimate(String climate) {
-        this.climate = climate;
-    }
-    
-    @Override
-    public boolean equals(Object obj) {
-        return EqualsBuilder.reflectionEquals(obj, this);
-    }
+  @Column(nullable = false)
+  private String terrain;
+
+  public Planet() {
+  }
+
+  public Planet(String climate, String terrain) {
+    this.climate = climate;
+    this.terrain = terrain;
+  }
+
+  public Planet(String name, String climate, String terrain) {
+    this.name = name;
+    this.climate = climate;
+    this.terrain = terrain;
+  }
+
+  public Planet(Long id, String name, String climate, String terrain) {
+    this.id = id;
+    this.name = name;
+    this.climate = climate;
+    this.terrain = terrain;
+  }
+
+  public Long getId() {
+    return id;
+  }
+
+  public void setId(Long id) {
+    this.id = id;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public void setName(String name) {
+    this.name = name;
+  }
+
+  public String getClimate() {
+    return climate;
+  }
+
+  public void setClimate(String climate) {
+    this.climate = climate;
+  }
+
+  public String getTerrain() {
+    return terrain;
+  }
+
+  public void setTerrain(String terrain) {
+    this.terrain = terrain;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    return EqualsBuilder.reflectionEquals(obj, this);
+  }
+
+  @Override
+  public String toString() {
+    return "Planet [climate=" + climate + ", id=" + id + ", name=" + name + ", terrain=" + terrain + "]";
+  }
+
 }
--- src/main/java/com/compass/sw_planet_api/web/PlanetController.java
+++ src/main/java/com/compass/sw_planet_api/web/PlanetController.java
@@ -1,11 +1,18 @@
 package com.compass.sw_planet_api.web;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.compass.sw_planet_api.domain.Planet;
@@ -14,12 +21,37 @@ import com.compass.sw_planet_api.service.PlanetService;
 @RestController
 @RequestMapping("/planets")
 public class PlanetController {
-    @Autowired
-    private PlanetService planetService;
+  @Autowired
+  private PlanetService planetService;
 
-    @PostMapping
-    public ResponseEntity<Planet> create(@RequestBody Planet planet) {
-        Planet planetCreated = planetService.create(planet);
-        return ResponseEntity.status(HttpStatus.CREATED).body(planetCreated);
-    }
+  @PostMapping
+  public ResponseEntity<Planet> create(@RequestBody @Validated Planet planet) {
+    Planet planetCreated = planetService.create(planet);
+    return ResponseEntity.status(HttpStatus.CREATED).body(planetCreated);
+  }
+
+  @GetMapping("/{id}")
+  public ResponseEntity<Planet> get(@PathVariable("id") Long id) {
+    return planetService.get(id).map(planet -> ResponseEntity.ok(planet))
+        .orElseGet(() -> ResponseEntity.notFound().build());
+  }
+
+  @GetMapping("/name/{name}")
+  public ResponseEntity<Planet> getByName(@PathVariable("name") String name) {
+    return planetService.getByName(name).map(planet -> ResponseEntity.ok(planet))
+        .orElseGet(() -> ResponseEntity.notFound().build());
+  }
+
+  @GetMapping
+  public ResponseEntity<List<Planet>> list(@RequestParam(required = false) String terrain,
+      @RequestParam(required = false) String climate) {
+    List<Planet> planets = planetService.list(terrain, climate);
+    return ResponseEntity.ok(planets);
+  }
+
+  @DeleteMapping("/{id}")
+  public ResponseEntity<Void> remove(@PathVariable("id") Long id) {
+    planetService.remove(id);
+    return ResponseEntity.noContent().build();
+  }
 }
